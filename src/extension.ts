ee62286eb3641cf2cdda64d01edf3443836af3f7
gusalbukrk <gusalbukrk@gmail.com>
2024-02-01 19:45:49 -0300

    add status bar item

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,25 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "extension" is now active!');
 
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from extension!');
+		vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	const sbi = vscode.window.createStatusBarItem(
+		'my.statusBar.item', // optional id, if not provided it will be the extension identifier
+		vscode.StatusBarAlignment.Left, // Left or Right
+		-100, // priority (the higher value means the item should be shown more to the left)
+	);
+	sbi.text = '$(megaphone) Hello World!';
+	sbi.name = 'Item name'; // will appear, for instance, in the context menu of the status bar
+	sbi.command = 'extension.helloWorld';
+	sbi.tooltip = 'Item tooltip';
+
+	// only colors supported: 'statusBarItem.errorBackground' & 'statusBarItem.warningBackground'
+	sbi.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground');
+
+	sbi.show();
 }
 
 export function deactivate() {}
